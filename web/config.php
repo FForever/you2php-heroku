ef16f98cf9f4e57c46a50a2bbe3f3564ab50c1d0
FForever <30847642+FForever@users.noreply.github.com>
2018-02-14 09:05:05 -0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('APIKEY', 'AIzaSyCJzA1K95AYi3QowQz-aVi83MvIusUJdd4');//这里改成您的api key
+define('GJ_CODE', 'US');//国家代码
+define('SITE_NAME', 'FFY2B');//网站名字
+define('TITLENAME', 'FFY2B');//网站名字
+define('EN2DEKEY', 'AnQU%P1XD!E@F435e67CtzD*$#Sh4');//一个随机字符串，加解密KEY
+define('EMAIL', 'remaoqi27@gmail.com');//邮箱
 ?>
